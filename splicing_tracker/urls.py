5c15cb22f523accb29173db96695dcdc0107cc66
BwalyaB <bukoloeb@gmail.com>
2026-04-15 12:38:37 +0200

    includes password reset urls

--- splicing_tracker/urls.py
+++ splicing_tracker/urls.py
@@ -3,12 +3,22 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Include Django's auth URLs for login/logout
+
+    # Password Reset flow - These will look for your templates in templates/registration/
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    # Include remaining Django auth URLs (login, logout, etc.)
     path('accounts/', include('django.contrib.auth.urls')),
-    # Include the splicing application's URLs as the root path
+
+    # Splicing application URLs
     path('', include('splicing.urls')),
 ]
 
@@ -17,7 +27,6 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # CRITICAL FIX: Serve Static files in production (when DEBUG=False)
-# This forces Django to find the CSS files even if the middleware fails.
 if not settings.DEBUG:
     urlpatterns += [
         re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
